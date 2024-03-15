52b2abf4b94da94b0bd59a3f3fb00ddcf52c8f43
Rameshwar Singh <89194728+ramuatgnes@users.noreply.github.com>
2024-03-15 12:01:16 +0530

    Data-binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StringInterpolationComponent } from './demoComponentForDataBinding/string-interpolation/string-interpolation.component';
+import { PropertyBindingComponent } from './demoComponentForDataBinding/property-binding/property-binding.component';
+import { EventBindingComponent } from './demoComponentForDataBinding/event-binding/event-binding.component';
+import { TwoWayBindingComponent } from './demoComponentForDataBinding/two-way-binding/two-way-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StringInterpolationComponent,
+    PropertyBindingComponent,
+    EventBindingComponent,
+    TwoWayBindingComponent
   ],
   imports: [
     BrowserModule,
